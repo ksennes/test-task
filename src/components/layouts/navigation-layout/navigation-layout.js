a09a0c4fa2ae1c00ac9d2a75889e60d2b1f2e0ec
ksennes <ksenneste1@gmail.com>
2020-09-03 20:35:15 +0300

    feat: add navigation layout

---
+++ src/components/layouts/navigation-layout/navigation-layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './navigation-layout.css';
+import logo from '../../../img/logo.png';
+
+export const NavigationLayout = () => {
+    return(
+        <div className='navigation-layout'>
+            <div className='navigation-layout__logo'>
+                <img src={logo}/>
+            </div>
+            <div className='navigation-layout__options'>
+
+            </div>
+        </div>
+    )
+}
