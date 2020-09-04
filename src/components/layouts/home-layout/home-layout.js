fe97d56c6a109618ec5020ce208884a6d7b4f635
ksennes <ksenneste1@gmail.com>
2020-09-04 15:32:32 +0300

    fear: add home layout

--- src/components/layouts/home-layout/home-layout.js
+++ src/components/layouts/home-layout/home-layout.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import "./home-layout.css";
+import "./home-layout.scss";
 import bear from '../../../img/bear-face.png';
+import bearMobile from '../../../img/bearFace.png';
+
 import { NavigationLayout } from "../navigation-layout/navigation-layout";
 
 export const HomeLayout = () => {
@@ -11,7 +13,8 @@ export const HomeLayout = () => {
       <div className="home-layout">
           <div className='home-layout__block'>
               <div className='home-layout__img'>
-                  <img src={bear}/>
+                  <img src={bear} className='bear'/>
+                  <img src={bearMobile} className='bear-mobile'/>
               </div>
               <button className='home-layout__button'>Let me in</button>
           </div>
