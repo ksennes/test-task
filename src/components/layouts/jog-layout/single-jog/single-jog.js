b0cec9360a2914163e0720ab8b09fd35e8fc7d09
ksennes <ksenneste1@gmail.com>
2020-09-06 11:33:59 +0300

    feat: fix navigation layout

--- src/components/layouts/jog-layout/single-jog/single-jog.js
+++ src/components/layouts/jog-layout/single-jog/single-jog.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./single-jog.scss";
 import icon from "../../../../img/icon.png";
 
-export const SingleJog = ({ date, speed, distance, time }) => {
+export const SingleJog = ({ date, distance, time }) => {
   return (
     <div className="single-jog">
       <div className="single-jog__icon">
@@ -11,9 +11,8 @@ export const SingleJog = ({ date, speed, distance, time }) => {
       </div>
       <div className="single-jog__details">
         <p className="date">{date}</p>
-        <h4>Speed: {speed}</h4>
-        <h4>Distance: {distance}</h4>
-        <h4>Time: {time}</h4>
+        <h4>Distance: <span>{distance}</span></h4>
+        <h4>Time: <span>{time}</span></h4>
       </div>
     </div>
   );
--- src/components/layouts/navigation-layout/navigation-layout.js
+++ src/components/layouts/navigation-layout/navigation-layout.js
@@ -6,10 +6,12 @@ import logo from "../../../img/logo.png";
 import greenLogo from '../../../img/logo1.png';
 import burger from '../../../img/menu.png';
 import cross from '../../../img/cross.png';
+import filter from '../../../img/filter.png';
+import activeFilter from '../../../img/filterActive.png';
 import { DesktopNavigation } from "./desktop-navigation/desktop-navigation";
 import { MobileNavigation } from "./mobile-navigation/mobile-navigation";
 
-export const NavigationLayout = ({ isLogged, activeLink }) => {
+export const NavigationLayout = ({ isLogged, activeLink, isFilterOpen, handleFilter }) => {
   const navigationList = [
     { title: "JOGS", link: "/jog" },
     { title: "INFO", link: "/info" },
@@ -35,6 +37,9 @@ export const NavigationLayout = ({ isLogged, activeLink }) => {
         {isLogged ? (
             <>
             <DesktopNavigation navigationList={navigationList} activeLink={activeLink}/>
+            <div className='filter__icon'>
+              <img src={isFilterOpen ? activeFilter : filter} onClick={handleFilter}/>
+            </div>
             <div className='burger' onClick={() => handleOpenMenu()} style={isOpen ? {display: 'none'} : null}>
                 <img src={burger}/>
             </div>
